3da0ef205f50b5ee5e684db4a5ca84da3e4e8686
zhengjinhan <jinhan.zheng@inmeijia.com>
2018-12-21 11:33:18 +0800

    publish

--- publish.sh
+++ publish.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 # 执行git命令 切到master合并分支代码，
-git_branch = `git symbolic-ref --short -q HEAD`
-echo $git_branch
-git checkout origin master
+git_branch=`git symbolic-ref --short -q HEAD`
+echo current branch is $git_branch
+git checkout master
 git pull origin master
 git pull origin $git_branch
 git push origin master
 # 切换回原来分支
-git checkout $git_branch
+# git checkout $git_branch
+# echo current branch building...
+# npm run build-prod
+# echo current branch build success
