9072c5e47eb6e481634e61439484a65d07567502
zhengjinhan <jinhan.zheng@inmeijia.com>
2018-12-21 11:23:52 +0800

    test shell

--- publish.sh
+++ publish.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 # 执行git命令 切到master合并分支代码，
 git_branch = `git symbolic-ref --short -q HEAD`
+echo $git_branch
 git checkout origin master
 git pull origin master
-git pull origin git_branch
+git pull origin $git_branch
 git push origin master
 # 切换回原来分支
-git checkout git_branch
+git checkout $git_branch
